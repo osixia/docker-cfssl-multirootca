330a5b248c9f4aa2243254ef824110718d0dcd41
ofreax <bertrand.gouny@gmail.com>
2016-01-19 20:59:19 +0100

    complex-bash-env

--- image/service/multirootca/startup.sh
+++ image/service/multirootca/startup.sh
@@ -14,7 +14,7 @@ if [ ! -e "$FIRST_START_DONE" ]; then
   # tls config
   if [ "${CFSSL_MUTLTIROOTCA_HTTPS,,}" == "true" ]; then
     log-helper info "Use HTTPS..."
-    
+
     # generate a certificate and key with cfssl tool if LDAP_CRT and LDAP_KEY files don't exists
     # https://github.com/osixia/docker-light-baseimage/blob/stable/image/service-available/:cfssl/assets/tool/cfssl-helper
     cfssl-helper ${CFSSL_MULTIROOTCA_CFSSL_PREFIX} "${CONTAINER_SERVICE_DIR}/multirootca/assets/certs/$CFSSL_MUTLTIROOTCA_HTTPS_CRT_FILENAME" "${CONTAINER_SERVICE_DIR}/multirootca/assets/certs/$CFSSL_MUTLTIROOTCA_HTTPS_KEY_FILENAME" "${CONTAINER_SERVICE_DIR}/multirootca/assets/certs/ca.crt"
@@ -38,27 +38,27 @@ if [ ! -e "$FIRST_START_DONE" ]; then
     do
       if [ $(complex-bash-env isRow "${!info}") = true ]; then
         local key=$(complex-bash-env getRowKey "${!info}")
-        local value=$(complex-bash-env getRowValue "${!info}")
+        local valueVarName=$(complex-bash-env getRowValueVarName "${!info}")
 
-        if [ $(complex-bash-env isTable "$value") = true ] || [ $(complex-bash-env isRow "$value") = true ]; then
-          ca_info "$to_print$key" "$value"
+        if [ $(complex-bash-env isTable "${!valueVarName}") = true ] || [ $(complex-bash-env isRow "${!valueVarName}") = true ]; then
+          ca_info "$to_print$key" "$valueVarName"
         else
-          append_value_to_file "$to_print$key" "$value"
+          append_value_to_file "$to_print$key" "${!valueVarName}"
         fi
       fi
     done
   }
 
-  for ca in $(complex-bash-env iterate "${CFSSL_MULTIROOTCA_ROOTS}")
+  for ca in $(complex-bash-env iterate CFSSL_MULTIROOTCA_ROOTS)
   do
     if [ $(complex-bash-env isRow "${!ca}") = true ]; then
       hostname=$(complex-bash-env getRowKey "${!ca}")
-      info=$(complex-bash-env getRowValue "${!ca}")
+      info=$(complex-bash-env getRowValueVarName "${!ca}")
 
       append_to_file "[ ${hostname} ]"
       ca_info "" "$info"
     else
-      append_to_file "[ ${ca} ]"
+      append_to_file "[ ${!ca} ]"
     fi
   done
 
